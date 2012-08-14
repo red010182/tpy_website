66c75a410f9609cbc37679a31332ea579810128d
tpy <p.truesword@gmail.com>
2012-08-14 10:45:35 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TpyWebsite::Application.config.secret_token = '151b1cb1a147acdea218145db880ad1850a58ec3218dff4eb064346f5b25aa254e4d3a3ee9c566959f7a1f5fecc73b545678ccd853a9f5257764f111b4d53a0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TpyWebsite::Application.config.session_store :cookie_store, key: '_tpy_website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TpyWebsite::Application.config.session_store :active_record_store
